3bbf3cdace7dbbc26a337cf9e684caf5370f105b
Amy Lee <al7798@g.rit.edu>
2026-09-02 12:50:59 -0400

    E02 - Library configuration
    Also deleted a duplicate of E02.

--- E02_Configuration/Main.h
+++ E02_Configuration/Main.h
@@ -1,11 +1,17 @@
 /*----------------------------------------------
-Programmer: Alberto Bobadilla (labigm@rit.edu)
+Programmer: Alberto Bobadilla (labigm@rit.edu) 
 Date: 2023/09
 ----------------------------------------------*/
 #ifndef __MAIN_H_
 #define __MAIN_H_
 
-#include <iostream>
-#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 #endif //__MAIN_H_
